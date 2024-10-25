159835bc1e0942332389985167a06c54f28526d9
benjamingohps <benjamingohps@gmail.com>
2024-10-25 10:02:46 +0800

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+// src/App.jsx
+
+const App = () => {
+  return (
+    <div className="container">
+      <QueryClientProvider client={queryClient}>
+        <Display />
+      </QueryClientProvider>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Display.jsx
@@ -0,0 +1,14 @@
+import { useQueryClient } from '@tanstack/react-query';
+import React from 'react';
+
+const Display = () => {
+    const queryClient = useQueryClient();
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Display;
